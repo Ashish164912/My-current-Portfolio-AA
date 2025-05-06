c75d5a9711c0546318fd696cf533343443c3a4a3
Ashish164912 <ashishausarmal@gmail.com>
2025-05-06 23:03:04 +0530

    javascript Page

---
+++ demo.js
@@ -0,0 +1,8 @@
+function button1(){
+  document.getElementById('nameinp').innerText=" Name : Ashish Dilip Ausarmal";
+  document.getElementById('emailinp').innerText="Email : ashishausarmal@gmail.com";
+  document.getElementById('ageinp').innerText="Age : 21";
+  document.getElementById('Mobileinp').innerText="Contact : 7558511590";
+  document.getElementById('placeinp').innerText="Place : Koradgaon";
+
+}
