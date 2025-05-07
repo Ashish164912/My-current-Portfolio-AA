a5b4dccdd1a38c5bc1d1186183dcd16d54b4590a
Ashish164912 <ashishausarmal@gmail.com>
2025-05-07 22:27:05 +0530

    js file

--- demo.js
+++ demo.js
@@ -6,3 +6,58 @@ function button1(){
   document.getElementById('placeinp').innerText="Place : Koradgaon";
 
 }
+
+function Readmore1(){
+  const card1=document.getElementById('card1js');
+  card1.style.backgroundColor="orange";
+  card1.style.borderRadius="20px";
+  card1.style.transform="translateY(-5px)";
+  card1.style.color="black";
+  card1.style.fontWeight="bold"
+ 
+ }
+
+function Readmore2(){
+  const card1=document.getElementById('card1js');
+  card1.style.backgroundColor="";
+  card1.style.borderRadius="";
+  card1.style.transform="";
+  card1.style.color="";
+   card1.style.fontWeight=""
+}
+function Readmore3(){
+  const card2=document.getElementById('card2js');
+  card2.style.backgroundColor="orange";
+  card2.style.borderRadius="20px";
+  card2.style.transform="translateY(-5px)";
+  card2.style.color="black";
+  card2.style.fontWeight="bold"
+ 
+ }
+
+function Readmore4(){
+  const card2=document.getElementById('card2js');
+  card2.style.backgroundColor="";
+  card2.style.borderRadius="";
+  card2.style.transform="";
+  card2.style.color="";
+   card2.style.fontWeight=""
+}
+function Readmore5(){
+  const card1=document.getElementById('card3js');
+  card1.style.backgroundColor="orange";
+  card1.style.borderRadius="20px";
+  card1.style.transform="translateY(-5px)";
+  card1.style.color="black";
+  card1.style.fontWeight="bold"
+ 
+ }
+
+function Readmore6(){
+  const card1=document.getElementById('card3js');
+  card1.style.backgroundColor="";
+  card1.style.borderRadius="";
+  card1.style.transform="";
+  card1.style.color="";
+   card1.style.fontWeight=""
+}
